640193ce1ae1daf6aca0d16f042a43f521fb1ace
Mohammad Ismail <ismail@jublia.com>
2019-07-24 18:02:12 +0700

    fix linter and add errors to logger

--- index.js
+++ index.js
@@ -1,21 +1,29 @@
-'use strict';
 
-const express = require('express');
-const app = express();
 const port = 8010;
-
-const bodyParser = require('body-parser');
-const jsonParser = bodyParser.json();
-
 const sqlite3 = require('sqlite3').verbose();
+
 const db = new sqlite3.Database(':memory:');
+const winston = require('winston');
+const app = require('./src/app');
+
+// Logger configuration
+const logConfiguration = {
+  transports: [
+    new winston.transports.File({
+      filename: './logs/example-2.log',
+    }),
+  ],
+};
+
+// Create the logger
+const logger = winston.createLogger(logConfiguration);
 
 const buildSchemas = require('./src/schemas');
 
 db.serialize(() => {
-    buildSchemas(db);
+  buildSchemas(db);
 
-    const app = require('./src/app')(db);
-    
-    app.listen(port, () => console.log(`App started and listening on port ${port}`));
+  const appRun = app(db);
+
+  appRun.listen(port, () => logger.info(`App started and listening on port ${port}`));
 });
